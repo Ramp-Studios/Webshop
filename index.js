a04f9d5c9a75cbd819d41194913c36fe05c352e2
Michael <MichaelCheungZakelijk@gmail.com>
2020-02-10 17:17:32 +0100

    0.15

--- index.js
+++ index.js
@@ -24,4 +24,10 @@ app.get('/api', (request, response) => {
     response.json(JSON.stringify({ response: 'hi'}));
 })
 
+const menu = document.querySelector("menu");
+const navlinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("open")
+});
