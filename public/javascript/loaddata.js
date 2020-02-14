7fca660da78c6ba16e5b7d810f1f3ba4cf304f7c
daaku <alexanderborgers@outlook.com>
2020-02-14 12:15:05 +0100

    fixed timer

--- public/javascript/loaddata.js
+++ public/javascript/loaddata.js
@@ -13,9 +13,9 @@ function dagdealtimer() {
     let timeleft = end - now;
     let hours = Math.floor(timeleft / 60 / 60 / 1000);
     timeleft = timeleft - hours * 60 * 60 * 1000;
-    let minutes = Math.floor(timeleft / 60 / 1000);
+    let minutes = Math.floor(timeleft / 60 / 1000) < 10 ? '0' + Math.floor(timeleft / 60 / 1000) : Math.floor(timeleft / 60 / 1000);
     timeleft = timeleft - minutes * 60 * 1000;
-    let seconds = Math.floor(timeleft / 1000);
+    let seconds = Math.floor(timeleft / 1000) < 10 ? '0' + Math.floor(timeleft / 1000) : Math.floor(timeleft / 1000);
     timer.innerHTML = `${hours}:${minutes}:${seconds}`;
 } 
 setInterval(() => {
